4a181720272e55e6efec0deec82f1fc8dcf3b199
herahdah <168187077+herahdah@users.noreply.github.com>
2024-09-23 20:46:33 +0100

    update

--- src/component/Explore.jsx
+++ src/component/Explore.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import image1 from '../assets/e1.jpg'
 
 export default function Explore() {
     const explore=[
         {
-            image:,
+            image:image1,
             tom:'Tommy Helfinger Bar',
-            
+
 
         }
 
@@ -17,6 +18,9 @@ export default function Explore() {
         <div className='bg-slate-300 h-[100vh] text-center pt-24'>
             <h1 className='uppercase text-3xl'>explore</h1>
             <p className='pt-7 text-xl text-slate-500'>Explore New place, food, culture around the world and many more</p>
+        </div>
+        <div>
+            <img src={image1} alt="" />
         </div>
     </div>
   )
